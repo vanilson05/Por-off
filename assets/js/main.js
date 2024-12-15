62bb355037f44ee0f976cdffc99a24e49d496bff
vanilson silva de souza <souzavanilson496@gmail.com>
2024-12-15 12:14:33 -0300

    att

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,27 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactmessage = document.getElementById('contact__message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    //serviceID - templeteID - #form - publickey
+    emailjs.sendForm('service_g4tj7rq','template_k93vfhn','#contact-form','J6ly-p_FWOUul1vwK')
+
+    .then(() =>{
+        //show sent message
+        contactmessage.textContent = 'Enviado com sucesso ✅'
+
+        // remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = '';
+        }, 5000);
+
+        //clear input fields
+        contactForm.reset()
+    })
+}
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
